6a9be668fedfae162ad340dcd621a9b97f00ef6e
Kevin <kevinhill@live.com>
2019-10-26 09:59:32 -0700

    updating spelling error

--- src/modules/c/app/__benchmarks__/app.benchmark.js
+++ src/modules/c/app/__benchmarks__/app.benchmark.js
@@ -1,10 +1,11 @@
 import { createElement } from 'lwc';
 import MyApp from 'my/app';
-import { runInContext } from 'vm';
 
 describe('my-app', () => {
-    benchmark('creat_and_render', () => {
+    // eslint-disable-next-line no-undef
+    benchmark('create_and_render', () => {
         let element;
+        // eslint-disable-next-line no-undef
         run(() => {
             element = createElement('my-app', { is: MyApp });
             element.flavor = 'red';
