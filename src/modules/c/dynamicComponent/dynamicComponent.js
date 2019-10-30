01841ec4c8dff351c1e37f89b2bd3544906598c6
Kevin <kevinhill@live.com>
2019-10-29 21:10:32 -0700

    adding cssOnlyModule

--- src/modules/c/dynamicComponent/dynamicComponent.js
+++ src/modules/c/dynamicComponent/dynamicComponent.js
@@ -1,4 +1,4 @@
-import { LightningElement, track, api } from 'lwc';
+import {LightningElement, track, api} from 'lwc';
 
 // const FORM_FACTOR_MODULES = {
 //     desktop: 'c/desktop',
@@ -7,44 +7,46 @@ import { LightningElement, track, api } from 'lwc';
 const DEFAULT_FORM_FACTOR = 'desktop';
 
 export default class DynamicComponent extends LightningElement {
-    @api formFactor = DEFAULT_FORM_FACTOR;
-    @track formFactorModule;
-    @track passedInValue = 'This is a value from the parent';
+  @api formFactor = DEFAULT_FORM_FACTOR;
+  @track formFactorModule;
+  @track passedInValue = 'This is a value from the parent';
 
-    // Now that the component is in the DOM start the process of loading a new module
-    connectedCallback() {
-        this.loadFormFactorModule();
+  title = 'Hello Title';
+
+  // Now that the component is in the DOM start the process of loading a new module
+  connectedCallback() {
+    this.loadFormFactorModule();
+  }
+
+  // Need to use the async pattern to load a module at runtime
+  async loadFormFactorModule() {
+    if (this.isMobile) {
+      const loadModule = await import('c/mobile');
+      this.formFactorModule = loadModule;
     }
 
-    // Need to use the async pattern to load a module at runtime
-    async loadFormFactorModule() {
-        if (this.isMobile) {
-            const loadModule = await import('c/mobile');
-            this.formFactorModule = loadModule;
-        }
-
-        if (this.isDesktop) {
-            const loadModule = await import('c/desktop');
-            this.formFactorModule = loadModule;
-        }
-
-        // const loadModule = await import(
-        //     `${FORM_FACTOR_MODULES[this.formFactor]}`
-        // );
-        // this.formFactor = loadModule;
+    if (this.isDesktop) {
+      const loadModule = await import('c/desktop');
+      this.formFactorModule = loadModule;
     }
 
-    get isMobile() {
-        if (this.formFactor === 'mobile') {
-            return true;
-        }
-        return false;
-    }
+    // const loadModule = await import(
+    //     `${FORM_FACTOR_MODULES[this.formFactor]}`
+    // );
+    // this.formFactor = loadModule;
+  }
 
-    get isDesktop() {
-        if (this.formFactor === 'desktop') {
-            return true;
-        }
-        return false;
+  get isMobile() {
+    if (this.formFactor === 'mobile') {
+      return true;
     }
+    return false;
+  }
+
+  get isDesktop() {
+    if (this.formFactor === 'desktop') {
+      return true;
+    }
+    return false;
+  }
 }
